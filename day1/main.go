dea9ede1056a508cf48a2b7364b0ff3918f53d1d
Brad Davies <brad.davies@withwise.com>
2021-12-01 05:48:30 +0000

    Day 1 - Part 1 & 2

---
+++ day1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+
+	"github.com/varbrad/advent-of-code-2021/utils"
+)
+
+func main() {
+	input, err := utils.ReadInputToIntegerList("day1/input")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	utils.Part1(Day1Part1(input))
+	utils.Part2(Day1Part2(input))
+}
+
+func Day1Part1(input []int) int {
+	return calculateIncreases(input)
+}
+
+func Day1Part2(input []int) int {
+	reducedList := []int{}
+	for i := 0; i < len(input)-2; i++ {
+		reducedList = append(reducedList, input[i]+input[i+1]+input[i+2])
+	}
+	return calculateIncreases(reducedList)
+}
+
+func calculateIncreases(numbers []int) int {
+	inc := 0
+
+	for i := 0; i < len(numbers)-1; i++ {
+		if numbers[i] < numbers[i+1] {
+			inc += 1
+		}
+	}
+
+	return inc
+}
---
+++ day1/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDay1Part1(t *testing.T) {
+	t.Run("Should solve the example", func(t *testing.T) {
+		input := []int{
+			199,
+			200,
+			208,
+			210,
+			200,
+			207,
+			240,
+			269,
+			260,
+			263,
+		}
+
+		expected := 7
+		actual := Day1Part1(input)
+
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestDay1Part2(t *testing.T) {
+	t.Run("Should solve the example", func(t *testing.T) {
+		input := []int{
+			199,
+			200,
+			208,
+			210,
+			200,
+			207,
+			240,
+			269,
+			260,
+			263,
+		}
+
+		expected := 5
+		actual := Day1Part2(input)
+
+		assert.Equal(t, expected, actual)
+	})
+}
